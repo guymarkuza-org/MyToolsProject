bc111872aa7141d6e9606ae6abf39f777843308b
Guy Markuza <guy.markuza@intel.com>
2020-05-11 17:57:17 +0300

    Add Tool1 and Calc

---
+++ Tools/Tool1/Calc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool1
+{
+    public class Calc
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a* b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
