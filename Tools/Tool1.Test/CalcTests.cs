920c08f868462467aa74719c11f8c93fe64c2a78
Guy Markuza <guy.markuza@intel.com>
2020-05-12 11:47:06 +0300

    Add UT Proj

---
+++ Tools/Tool1.Test/CalcTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tool1.Test
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void TestCalcAdd()
+        {
+            int sum = Calc.Add(4, 5);
+            Assert.AreEqual(9, sum, "Not equal");
+        }
+    }
+}
---
+++ Tools/Tool1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tool1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tool1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3edf6771-51fa-46d0-a61e-48fa3dfdd7ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
